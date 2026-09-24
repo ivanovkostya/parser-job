07df4a1c12f20e50a5ce2c9a0c195f9b4885908b
Konstantin Ivanov <ivanov.k@citilink.ru>
2026-09-24 20:23:08 +0300

    init commit

---
+++ src/main/java/org/example/parserjob/ParserJobApplication.java
@@ -0,0 +1,13 @@
+package org.example.parserjob;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParserJobApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ParserJobApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/parserjob/ParserJobApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.parserjob;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParserJobApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
